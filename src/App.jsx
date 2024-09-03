397cdf6518ff8f37cf95f3ffec958cb0ef8e2f7b
rehanqasimk <qkrehan@gmail.com>
2024-09-03 15:19:30 +0500

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return <p>Hello From Rehan Qasim</p>;
+  return <p>Hello From Rehan Qasim. The Content is coming soon.</p>;
 };
 
 const container = document.getElementById("root");
